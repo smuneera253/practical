a76e7956595f1215d4cde2f79b2e955fcf9e70b8
anusha <anushaelluri30@gmail.com>
2022-12-04 21:19:06 +0530

    yeah muneera your code is done please check the code

--- code.js
+++ code.js
@@ -1,26 +1,1 @@
-let factor=0;
-    for(let i=1;i<=num;i++){
-        if(num%i===0){
-            factor++;
-        }
-    }
-    if(factor==2){
-        console.log("Yes");
-    }else{
-        console.log("No");
-    }
-
-//palindrome
-
-let flage=0;
-    for(let i=0;i<=N && N!=0;i++){
-        if(str[i]!= str[N-i-1]){
-            flage=1;
-            break;
-        }
-    }
-    if(flage==0){
-        console.log("Yes");
-    }else{
-        console.log("No")
-    }
+let factor=0;    for(let i=1;i<=num;i++){        if(num%i===0){            factor++;        }    }    if(factor==2){        console.log("Yes");    }else{        console.log("No");    }//palindromelet flage=0;    for(let i=0;i<=N/2 && N!=0;i++){        if(str[i]!= str[N-i-1]){            flage=1;            break;        }    }    if(flage==0){        console.log("Yes");    }else{        console.log("No")    }
